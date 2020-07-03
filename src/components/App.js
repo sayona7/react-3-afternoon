e069260425e473e01de71061d53373e344b77a44
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-03 07:49:38 -0500

    solved steps 1-3

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import Post from "./Post/Post";
 
 import './App.css';
 
@@ -19,11 +21,17 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get("https://practiceapi.devmountain.com/api/posts")
+    .then( results => {
+      this.setState({posts: results.data});
+    });
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(results => {
+      this.setState({posts: results.data});
+    });
   }
 
   deletePost() {
@@ -44,6 +52,14 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
+
+          {posts.map(post => (
+            <Post key={post.id} 
+                  text={post.text}
+                  date={post.date}
+                  id={post.id}
+                  updatePostFn={this.updatePost} />
+          ))}
           
         </section>
       </div>
